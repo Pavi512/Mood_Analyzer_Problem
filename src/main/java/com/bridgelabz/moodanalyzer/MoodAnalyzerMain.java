a9c559a398927ef9bfb62e76c396338a659eb543
Pavi512 <gondipavithra@gmail.com>
2026-07-19 20:33:08 +0530

    [Pavithra] : Main class od mood analyzer

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzerMain.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzerMain.java
@@ -4,7 +4,7 @@ public class MoodAnalyzerMain {
 
     public static void main(String[] args) {
         // Create object using parameterized constructor
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
 
         // Display Mood
         System.out.println(moodAnalyzer.analyseMood());
