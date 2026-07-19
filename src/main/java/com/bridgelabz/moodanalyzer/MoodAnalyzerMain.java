1b0a18b8cc2a25afe00dd7ae5d202afad6b9887d
Pavi512 <gondipavithra@gmail.com>
2026-07-19 20:54:22 +0530

    [Pavithra] : Updated main class to handle MoodAnalysisException

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzerMain.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzerMain.java
@@ -3,10 +3,12 @@ package com.bridgelabz.moodanalyzer;
 public class MoodAnalyzerMain {
 
     public static void main(String[] args) {
-        // Create object using parameterized constructor
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-
-        // Display Mood
-        System.out.println(moodAnalyzer.analyseMood());
+        try {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
+            System.out.println(moodAnalyzer.analyseMood());
+        }
+        catch (MoodAnalysisException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
