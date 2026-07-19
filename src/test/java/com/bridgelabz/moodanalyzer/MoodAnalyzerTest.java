478137d5499fa0f4ce43441a94fdaf9741372efd
Pavi512 <gondipavithra@gmail.com>
2026-07-19 21:00:49 +0530

    [Pavithra] : Added JUnit test cases for null and empty mood exceptions

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -3,27 +3,45 @@ package com.bridgelabz.moodanalyzer;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MoodAnalyzerTest {
     // After refactoring code
     //Check for sad mood
     @Test
-    void sadMood() {
+    void sadMood() throws MoodAnalysisException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
         assertEquals("SAD", moodAnalyzer.analyseMood());
     }
 
     //Check for happy mood
     @Test
-    void happyMood() {
+    void happyMood() throws MoodAnalysisException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
         assertEquals("HAPPY", moodAnalyzer.analyseMood());
     }
 
-    //Test for null pointer exception is thrown
+
     @Test
-    void nullMood() {
+    void givenNullMood_ShouldThrowException() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-        assertEquals("Null value", moodAnalyzer.analyseMood());
+        MoodAnalysisException exception =
+                assertThrows(MoodAnalysisException.class,
+                        moodAnalyzer::analyseMood);
+        assertEquals(MoodAnalysisException.ExceptionType.NULL_MOOD,
+                exception.getType());
+    }
+
+    @Test
+    void givenEmptyMood_ShouldThrowException() {
+
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("");
+
+        MoodAnalysisException exception =
+                assertThrows(MoodAnalysisException.class,
+                        moodAnalyzer::analyseMood);
+        assertEquals(MoodAnalysisException.ExceptionType.EMPTY_MOOD,
+                exception.getType());
     }
 }
+
