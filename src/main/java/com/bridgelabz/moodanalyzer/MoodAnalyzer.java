390a7b3d1f76ea3fe7af85a7c0aec2562420bf85
Pavi512 <gondipavithra@gmail.com>
2026-07-19 20:22:45 +0530

    [Pavithra] : Added mood analyzer class

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.moodanalyzer;
+
+//Refactor code added constructors
+public class MoodAnalyzer {
+    private String message;
+
+    // Default Constructor
+    public MoodAnalyzer() {
+    }
+
+    // Parameterized Constructor
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    // Analyse mood
+    public String analyseMood() {
+        // Check whether the message contains Sad
+        if (message.contains("Sad")) {
+            return "SAD";
+        }
+        // Otherwise return HAPPY
+        return "HAPPY";
+    }
+}
